d4971c8c74c0da2e6386363b1e67a7312890e759
SpyBurner <zombiepigman1505@gmail.com>
2024-10-16 18:10:08 +0700

    Base done

--- Game.cpp
+++ Game.cpp
@@ -51,22 +51,26 @@ void Game::objectInit() {
 
     std::cout << "Object Initialisation..." << std::endl;
 
-    // Test scene
-    Scene *main = new Scene("Main");
+    #pragma region TEST ANIMATOR
+    // // Test scene
+    // Scene *main = new Scene("Main");
 
-    player->transform.position = Vector2(150, 200);
-    player->transform.scale = Vector2(5, 5);
-    player->AddComponent(new SpriteRenderer(player, renderer, Vector2(35, 44)));
+    // player->transform.position = Vector2(150, 200);
+    // player->transform.scale = Vector2(5, 5);
+    // player->AddComponent(new SpriteRenderer(player, renderer, Vector2(35, 44)));
 
-    SpriteRenderer *spriteRenderer = player->GetComponent<SpriteRenderer>();
+    // SpriteRenderer *spriteRenderer = player->GetComponent<SpriteRenderer>();
 
-    AnimationClip playerIdle = AnimationClip("Idle", "Assets/kirby_fall.png", Vector2(35, 44), 2000, false, 1.0, 0, 14);
-    AnimationClip playerFloat = AnimationClip("Float", "Assets/kirby_float.png", Vector2(35, 44), 2000, false, 1.0, 0, 3);
+    // AnimationClip playerIdle = AnimationClip("Idle", "Assets/kirby_fall.png", Vector2(35, 44), 2000, false, 1.0, 0, 14);
+    // AnimationClip playerFloat = AnimationClip("Float", "Assets/kirby_float.png", Vector2(35, 44), 2000, false, 1.0, 0, 3);
     
-    player->AddComponent(new Animator(player, {playerIdle, playerFloat}));
+    // player->AddComponent(new Animator(player, {playerIdle, playerFloat}));
 
-    SceneManager::GetInstance()->AddScene(main);
-    SceneManager::GetInstance()->LoadScene("Main");
+    // SceneManager::GetInstance()->AddScene(main);
+    // SceneManager::GetInstance()->LoadScene("Main");
+    #pragma endregion
+
+
 }
 
 void Game::handleEvents() {
@@ -91,29 +95,31 @@ GameObject* previousObject = player;
 void Game::update() {
     // Game logic updates go here
 
+    #pragma region TEST ANIMATOR
     // TEST INSTANTIATE
-    if (SDL_GetTicks() - lastSpawnTime >= spawnCooldown) {
-        GameObject *newObject = GameObject::Instantiate("Player" + std::to_string(spawnCount), 
-        *player, previousObject->transform.position + Vector2(30, 0), 0, Vector2(5, 5));
-        lastSpawnTime = SDL_GetTicks();
-        previousObject = newObject;
-        spawnCount++;
+    // if (SDL_GetTicks() - lastSpawnTime >= spawnCooldown) {
+    //     GameObject *newObject = GameObject::Instantiate("Player" + std::to_string(spawnCount), 
+    //     *player, previousObject->transform.position + Vector2(30, 0), 0, Vector2(5, 5));
+    //     lastSpawnTime = SDL_GetTicks();
+    //     previousObject = newObject;
+    //     spawnCount++;
 
-        newObject->GetComponent<Animator>()->GetClip("Idle")->onComplete->addHandler(
-            [newObject]() {
-                newObject->GetComponent<Animator>()->Play("Float");
-            }
-        );
+    //     newObject->GetComponent<Animator>()->GetClip("Idle")->onComplete->addHandler(
+    //         [newObject]() {
+    //             newObject->GetComponent<Animator>()->Play("Float");
+    //         }
+    //     );
 
-        newObject->GetComponent<Animator>()->GetClip("Float")->onComplete->addHandler(
-            [newObject]() {
-                newObject->GetComponent<Animator>()->Play("Idle");
-            }
-        );
+    //     newObject->GetComponent<Animator>()->GetClip("Float")->onComplete->addHandler(
+    //         [newObject]() {
+    //             newObject->GetComponent<Animator>()->Play("Idle");
+    //         }
+    //     );
 
-        newObject->GetComponent<Animator>()->Play("Idle");
-    }
+    //     newObject->GetComponent<Animator>()->Play("Idle");
+    // }
     //
+    #pragma endregion
 
     SceneManager::GetInstance()->Update();
 }
